515f38e578efe6c91e136757fd890355139d1582
Roy Hwang <tupaclypse@gmail.com>
2017-03-23 20:00:51 -0700

    Add upvotes and title to posts objects

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ var app = angular.module('MEANReddit', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello World!';
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 3},
+    {title: 'post 3', upvotes: 32},
+    {title: 'post 4', upvotes: 23},
+    {title: 'post 5', upvotes: 12}
+  ];
 }]);
