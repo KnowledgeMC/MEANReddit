1eec01ed9b35e8bedb803d874156fec29d704ef4
Roy Hwang <tupaclypse@gmail.com>
2017-03-27 19:19:32 -0700

    Created service and moved posts variable into it. Added comments

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var app = angular.module('MEANReddit', []);
 
+//creating a factory for posts
+
+// Creating a new object that has an array property called "posts"
+//We then return "o" so that object becomes exposed to any other
+//Angular module that cares to inject it. We could've exported array
+//directly but by exporting an object containing the posts array
+//we can add new objects and methbods to our services in the future.
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 3},
-    {title: 'post 3', upvotes: 32},
-    {title: 'post 4', upvotes: 23},
+    {title: 'post 3', upvotes: 22},
+    {title: 'post 4', upvotes: 8},
     {title: 'post 5', upvotes: 12}
   ];
   $scope.addPost = function(){
