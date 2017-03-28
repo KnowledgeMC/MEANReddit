60b071760c098d80365fadd83bc5e70c5d50f492
Roy Hwang <tupaclypse@gmail.com>
2017-03-27 19:38:15 -0700

    Created config block and configured a home state

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 var app = angular.module('MEANReddit', ['ui.router']);
 
-//creating a factory for posts
+//Creating config block and configure a home state. Use otherwise() to
+//redirect to unspecified routes.
+//Here we set up our home route. You'll notice that the state is given
+// a name ('home'), URL ('/home'), and template URL ('/home.html').
+//We've also told Angular that our new state should be controlled by
+//MainCtrl. Finally, using the otherwise() method we've specified what
+//should happen if the app receives a URL that is not defined. All
+//that's left to do is define the home.html template. Instead of
+//creating a new file, we are going to move most of our HTML into an inline template.
 
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
+
+//Creating a factory for posts:
 // Creating a new object that has an array property called "posts"
 //We then return "o" so that object becomes exposed to any other
 //Angular module that cares to inject it. We could've exported array
